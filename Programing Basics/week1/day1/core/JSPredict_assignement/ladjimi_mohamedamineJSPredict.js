3dd9c649f7fe7ecaabdea5d12f94afe59ff65207
mohamedamineladjimi <amineladjimi116@gmail.com>
2024-12-10 09:44:36 +0100

    extensionassig

---
+++ Programing Basics/week1/day1/core/JSPredict_assignement/ladjimi_mohamedamineJSPredict.js
@@ -0,0 +1,4 @@
+// prediction
+// 1-when console.log going to be called it will show "born in 1980"
+// 2-when console.log going to be called it will show "born in 1980"
+// 3- when console.log is called it will show 30 
