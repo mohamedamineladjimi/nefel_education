594ab25f6cf620e086ec8fcf8e33ea9a365f9c12
mohamedamineladjimi <amineladjimi116@gmail.com>
2024-12-11 08:37:57 +0100

    comment

---
+++ Programing Basics/week1/day2/optional/commenting practice/code.js
@@ -0,0 +1,8 @@
+//  this variable hold information which is 0 - what would this variable do?
+var likeCount = 0;
+// the function of this block of code is to calculate - what would be the function of this code block?
+function increaseLikes() {
+    // this is an addition operation  - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
+
