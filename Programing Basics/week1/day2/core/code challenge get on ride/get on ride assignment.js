dc4658132c05c362a819c083d6662cc3388b9ca9
mohamedamineladjimi <amineladjimi116@gmail.com>
2024-12-12 16:34:13 +0100

    rideon

---
+++ Programing Basics/week1/day2/core/code challenge get on ride/get on ride assignment.js
@@ -0,0 +1,20 @@
+var minimumheight = 42;
+var minimumage = 10;
+var a=50;
+var b=16;
+ //stretch 1 :
+/*if ( a >= minimumheight && b >= minimumage){
+    console.log ("Get on that ride, kiddo!")
+} else  { console.log ("Sorry kiddo. Maybe next year")
+
+    }*/
+     
+    //stretch 2:
+    if ( a >= minimumheight || b >= minimumage){
+        console.log ("Get on that ride, kiddo!")
+    } else  { console.log ("Sorry kiddo. Maybe next year")
+    
+        }
+        
+    
+     
