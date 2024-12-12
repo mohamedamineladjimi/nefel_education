1b1cf57ff1a1a7dbc1ae2ec763fdb7d9f9256dee
mohamedamineladjimi <amineladjimi116@gmail.com>
2024-12-12 23:18:33 +0100

    loop

---
+++ Programing Basics/week1/day2/practice/loopdloopassignment/ladjimi_mohamedamineloopdloop.js
@@ -0,0 +1,13 @@
+//step1:
+/* we know that we need a loop when we want to same action many times*/
+/* the starting point of  the loop is the part that define when the loop need to start/*
+/*the loop stop when the condition given is done */
+/* the loop know when to stop when we give her the stop point */ 
+/* incrementing is used to see if we still need the loop or no */
+/* the variables we need are the one that the loop is going to work on them */
+
+
+//step1:
+for (var i=2;i<=6;i+=2) {
+    console.log (i) }
+    
