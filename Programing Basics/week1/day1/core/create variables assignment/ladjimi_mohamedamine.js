73b174e7b64b6639b532b0a97f43c67e87c76174
mohamedamineladjimi <amineladjimi116@gmail.com>
2024-12-16 13:18:16 +0100

    webfundamental

--- Programing Basics/week1/day1/core/create variables assignment/ladjimi_mohamedamine.js
+++ Programing Basics/week1/day1/core/create variables assignment/ladjimi_mohamedamine.js
@@ -1,2 +1,20 @@
 var minimumheight = 42;
-var minimum age = 10;
+var minimumage = 10;
+var a=50;
+var b=16;
+ //stretch 1 :
+/*if ( a >= minimumheight && b >= minimumage){
+    console.log ("Get on that ride, kiddo!")
+} else  { console.log ("Sorry kiddo. Maybe next year")
+
+    }*/
+     
+    //stretch 2:
+    if ( a >= minimumheight || b >= minimumage){
+        console.log ("Get on that ride, kiddo!")
+    } else  { console.log ("Sorry kiddo. Maybe next year")
+    
+        }
+        
+    
+     
