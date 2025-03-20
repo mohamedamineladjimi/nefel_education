f92128bb15905b21abed21b4b22456eb30b46358
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-03-20 20:53:38 +0100

    forloopbasic1

---
+++ Python/week1/Core/for_loop_basic1_assignment/For-loop-basic-assingment.py
@@ -0,0 +1,16 @@
+for number in range(0,151):
+    print(number)
+    for i in range (5,1001):
+     if i %5==0:
+        print(i)
+
+    for i in range (1, 101):
+        if i %5==0:
+           print("coding")
+        if i %10==0:
+           print("Coding Dojo")
+        sum=0
+        for i in range (0,500001,2):
+            sum+=i
+              
+    
