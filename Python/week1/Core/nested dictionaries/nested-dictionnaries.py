c963fdbdd1799f049ba6c3fdf69de510b17dcc1b
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-03-25 15:45:22 +0100

    nested-dictionary

---
+++ Python/week1/Core/nested dictionaries/nested-dictionnaries.py
@@ -0,0 +1,11 @@
+#1 change the value:
+x[1][0]=15
+print(x)
+#2 change the last name :
+students[0]['last_name']= "bryant"
+#3 change messi to andres
+sport_directory['soccer'][0]="andres"
+print (sport_directory['soccer'])
+#4 change the value:
+z[0]['y']=30
+print(z)
