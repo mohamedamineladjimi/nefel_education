bcc0ac38ff187cf3603a3fed17ffa56c84939b88
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-04-24 12:32:25 +0100

    BankAccount

--- Python/week2/core/bank account assignment/bankaccount.py
+++ Python/week2/core/bank account assignment/bankaccount.py
@@ -6,20 +6,23 @@ class BankAccount:
         self.balance += amount
         return self
     def withdraw (self,amount):
-        if(self.balance = amount )>=0:
+        if(self.balance - amount )>=0:
             self.balance -= amount
         else:
             print("Insufficient Funds:charging a fee of 5$ ")
             self.balance -= 5
             return self
-        def display_account_info (self)
+    def display_account_info(self):
             print(f"Balance:{self.balance}")
             return self
-        def yeild_interest(self):
+    def yeild_interest(self):
             if self.balance > 0:
                 self.balance += (self.balance * self.int_rate)
             return self
-        
+savings= BankAccount(.05,1000)
+checking =BankAccount(.02,5000)
+
+savings.deposit(60).deposit(10).deposit(10).withdraw(400).yeild_interest().display_account_info()
 
             
 
