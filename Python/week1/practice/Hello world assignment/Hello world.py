50bacbc362712d86b5764c3d68dc98d2c18c74d2
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-05-13 18:26:39 +0100

    testing python

--- Python/week1/practice/Hello world assignment/Hello world.py
+++
@@ -1,8 +0,0 @@
-name = "Mohamed Amine Ladjimi"
-print ("Hello world")
-print("Hello " + name)
-number = str(2011)
-print("Hello " +  str(number) )
-food1 = "pizza"
-food2 = "mlewi"
-print("i love to eat  {food1} and {food2}") 
