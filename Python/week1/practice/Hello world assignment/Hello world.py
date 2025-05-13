622a093b56e7f3fad265fc0bb860f29f74404e2b
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-05-13 18:46:54 +0100

    Hello world assignment

---
+++ Python/week1/practice/Hello world assignment/Hello world.py
@@ -0,0 +1,9 @@
+name = "Mohamed Amine Ladjimi"
+print ("Hello world")
+print("Hello " + name)
+number = str(2011)
+print("Hello " +  str(number) )
+food1 = "pizza"
+food2 = "mlewi"
+print("i love to eat {} and {}" .format(food1,food2))
+print(f"i love to eat {food1} and {food2}") 
