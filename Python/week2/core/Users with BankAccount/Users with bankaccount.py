7eb5f01a10554d002932272ba3cc7cb6626d8c13
mohamedamineladjimi <amineladjimi116@gmail.com>
2025-05-19 16:42:11 +0100

    Users with BankAccount

---
+++ Python/week2/core/Users with BankAccount/Users with bankaccount.py
@@ -0,0 +1,45 @@
+class BankAccount:
+    def __init__(self, int_rate, balance): 
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+        else:
+            print("Insufficient funds: Charging a $5 fee")
+            self.balance -= 5
+        return self
+
+    def display_account_info(self):
+        print(f"Balance: ${self.balance}")
+        return self
+
+    def yield_interest(self):
+        if self.balance > 0:
+            self.balance += self.balance * self.int_rate
+        return self
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.account = BankAccount(int_rate=0.02, balance=0)
+
+    def make_deposit(self, amount):
+        self.account.deposit(amount)
+        return self
+
+    def make_withdrawal(self, amount):
+        self.account.withdraw(amount)
+        return self
+
+    def display_user_balance(self):
+        print(f"{self.name}'s Account:", end=" ")
+        self.account.display_account_info()
+        return self
+user1 = User("Alice", "alice@example.com")
+user1.make_deposit(100).make_withdrawal(20).display_user_balance()
